5f570520bfa00b2606016365bfb19016004a4a42
Jay Patrick Cano (It’s not a bug, it’s a feature) <0x3EF8@gmail.com>
2022-03-30 02:51:07 +0800

    Add files via upload

---
+++ WebFucker/Forms/aboutCoder.vb
@@ -0,0 +1,23 @@
+﻿Public Class aboutCoder
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://www.facebook.com/0x3EF8/")
+
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://0x3ef8.github.io/")
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        Process.Start("https://github.com/0x3EF8")
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
